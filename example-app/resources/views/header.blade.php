6633ce951f872789ee66664de73ba08504f4015f
peterpaleev <petpal005@gmail.com>
2022-10-24 21:49:50 +0300

    header and promo first steps

---
+++ example-app/resources/views/header.blade.php
@@ -0,0 +1,26 @@
+@include('head')
+<header>
+    <div class="navbar-wrapper">
+        <div class="navbar-logo"></div>
+        <div class="navbar-menu">
+            <a class="nav-item">
+                Главная
+            </a>
+            <a class="nav-item">
+                О нас
+            </a>
+            <a class="nav-item">
+                Производство
+            </a>
+            <a class="nav-item">
+                Оборудование
+            </a>
+            <a class="nav-item">
+                Проекты
+            </a>
+            <a class="nav-item">
+                Контакты
+            </a>
+        </div>
+    </div>
+</header>
