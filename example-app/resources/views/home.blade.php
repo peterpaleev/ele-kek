5816aee7f25bf8fe4649d105fb1065a654695610
peterpaleev <petpal005@gmail.com>
2022-10-25 10:53:53 +0300

    home pahe init

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -1,2 +1,55 @@
 @include('header')
 @include('promo')
+<h1 class="home-header">
+    Преимущества компании
+</h1>
+<div class="home-block-wrapper">
+    <div class="home-block">
+        <h1 class="home-block-header">
+            Наша история
+        </h1>
+        <span class="home-block-desc">
+            Компания существует с 1996 года и обладает внушительным опытом в деле производства и поставки промышленного оборудования и продукции клиентам в разных странах мира.
+        </span>
+    </div>    
+    <div class="home-block">
+        <h1 class="home-block-header">
+            Наша фабрика
+        </h1>
+        <span class="home-block-desc">
+            Собственный завод был запущен в Шанхае в 2006 году. Сейчас все процессы на ней идеально отлажены и настроены.
+        </span>
+    </div>    
+    <div class="home-block">
+        <h1 class="home-block-header">
+            Широкая география присутствия
+        </h1>
+        <span class="home-block-desc">
+            Мы поставляем нашу продукцию в страны Европы, Америки, Азии, Африки и Ближнего Востока, включая Канаду, США, Мексику, Чили, Бразилию. 
+        </span>
+    </div>    
+    <div class="home-block">
+        <h1 class="home-block-header">
+            Большой ассортимент продукции
+        </h1>
+        <span class="home-block-desc">
+            Мы выпускаем множество продуктов от лабораторных мельниц и диссольверов до готовых производственных линий.
+        </span>
+    </div>    
+    <div class="home-block">
+        <h1 class="home-block-header">
+            Передовое оборудование
+        </h1>
+        <span class="home-block-desc">
+            Наша фабрика оснащена самыми современными станками, сборочными линями, оборудованием для тестирования готовой продукции. 
+        </span>
+    </div>    
+    <div class="home-block">
+        <h1 class="home-block-header">
+            Наш сервис
+        </h1>
+        <span class="home-block-desc">
+            Компания не только производит и поставляет оборудование, но и осуществляет монтаж, сервис о после продажное обслуживание.
+        </span>
+    </div>
+</div>
